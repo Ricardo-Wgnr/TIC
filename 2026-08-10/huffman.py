31f6d95b70514bde648592fcbcff7263897c25ec
Ricardo Wagner <114697430+Ricardo-Wgnr@users.noreply.github.com>
2026-08-12 08:05:57 -0300

    Add files via upload

--- 2026-08-10/huffman.py
+++ 2026-08-10/huffman.py
@@ -17,9 +17,11 @@ print(f"Entropia: {komm.entropy(pmf):.2f}")
 code = komm.HuffmanCode(pmf)
 print(f"Taxa dp codigo: {code.rate(pmf):.2f}")
 
+# compressão
 codificado = code.encode(entrada)
+size = len(codificado)
 # pad para multiplo de 8
-codificado = np.concatenate([codificado, [0,0,0]])
+codificado = np.pad(codificado, (0,8 - size % 8))
 
 with open("alice.huff", "wb") as f:
     bytes = komm.bits_to_int(codificado.reshape(-1, 8))
@@ -29,3 +31,16 @@ with open("alice.huff", "wb") as f:
 print(f"Tamanho original (bits): {len(entrada)*8}")
 print(f"Tamanho comprimido (bits): {len(codificado)}")
 
+# descompressão
+
+with open("alice.huff", "rb") as f:
+    bytes = list(f.read())
+
+bits = komm.int_to_bits(bytes, width=8).reshape(-1)
+bits = bits[:size]                                      # remove o pad
+saida = code.decode(bits)
+print(saida)
+
+with open("alice2.txt", "wb") as f:
+    bytes = saida.astype(np.uint8)
+    f.write(bytes)
